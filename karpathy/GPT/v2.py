2a31a47fdf39cf9b9d3d269cb136ae152e4bc778
Emily Zuo <xiang.zuo@teradata.com>
2026-02-14 19:46:07 -0800

    updated

--- karpathy/GPT/v2.py
+++ karpathy/GPT/v2.py
@@ -3,14 +3,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 32 # how many independent sequences will we process in parallel?
-block_size = 8 # what is the maximum context length for predictions?
+batch_size = 64 # how many independent sequences will we process in parallel?
+block_size = 256 # what is the maximum context length for predictions?
 max_iters = 5000
-eval_interval = 300
-learning_rate = 1e-3
+eval_interval = 500
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32
+n_embd = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
 # ------------
 
 torch.manual_seed(1337)
@@ -43,6 +46,7 @@ class Head(nn.Module):
         self.query = nn.Linear(n_embd, head_size, bias=False)
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
         
     def forward(self, x):
         # input of size (batch, time-step, channels)
@@ -67,6 +71,7 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
         
     def forward(self, x):
         out = torch.cat([h(x) for h in self.heads], dim=-1)
@@ -80,7 +85,8 @@ class FeedForward(nn.Module):
         self.net = nn.Sequential(
             nn.Linear(n_embd, 4 * n_embd),
             nn.ReLU(), 
-            nn.Linear(4 * n_embd, n_embd),         
+            nn.Linear(4 * n_embd, n_embd),  
+            nn.Dropout(dropout),       
         )
     def forward(self, x): 
         return self.net(x)
@@ -92,11 +98,30 @@ class Block(nn.Module):
         head_size = n_embd // n_head
         self.sa = MultiHeadAttention(n_head, head_size)
         self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
         
     def forward(self, x):
-        x = x + self.sa(x)
-        x = x + self.ffwd(x)
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
         return x
+
+class BatchNorm1d:
+    def __init__(self, dim, eps=1e-5):
+        self.eps = eps
+        self.gamma = torch.ones(dim)
+        self.beta = torch.zeros(dim)
+    
+    def __call__(self, x):
+        # calculate the forward pass
+        xmean = x.mean(0, keepdim=True) # (1, T, C)
+        xvar = x.var(0, keepdim=True) # (1, T, C)
+        xhat = (x - xmean) / torch.sqrt(xvar + self.eps) # (B, T, C)
+        out = self.gamma * xhat + self.beta # (B, T, C)
+        return out
+    
+    def parameters(self):
+        return [self.gamma, self.beta]
 
 # data loading
 def get_batch(split):
@@ -130,10 +155,13 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.blocks = nn.Sequential(Block(n_embd, n_head=4),
-                                    Block(n_embd, n_head=4),
-                                    Block(n_embd, n_head=4),
-                                  )
+        # self.blocks = nn.Sequential(Block(n_embd, n_head=4),
+        #                             Block(n_embd, n_head=4),
+        #                             Block(n_embd, n_head=4),
+        #                             nn.LayerNorm(n_embd),
+        #                           )
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd) # final layer norm
        
         # self.sa_heads = MultiHeadAttention(4, n_embd//4)
         # self.ffwd = FeedForward(n_embd)
